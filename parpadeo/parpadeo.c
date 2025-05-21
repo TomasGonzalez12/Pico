7987356887e41ceacbd76476a3a97c3b1ac789b1
tomas <tomasgonzalez1291@gmal.com>
2025-05-21 18:23:47 -0300

    Proy Parpadeo agregado

---
+++ parpadeo/parpadeo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define LED_PIN 22
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_xor_mask(1 << LED_PIN);
+        sleep_ms(333);
+    }
+}
