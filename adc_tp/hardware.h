3e88152089fc496b07eada6dd0b5e002dd7e6c82
tomas <tomasgonzalez1291@gmal.com>
2025-08-18 07:12:32 -0300

    pre

--- adc_tp/hardware.h
+++ adc_tp/hardware.h
@@ -1,6 +1,8 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
+#define REBOTE_PULS 30
+
 #define LEDR_PIN 11
 #define LEDA_PIN 12
 #define LEDV_PIN 13
@@ -10,11 +12,20 @@
 #define PULS_PIN 18
 #define CTRL_BOMBA 16
 
-#define nivel_max_sup x
-#define nivel_min_sup y
+// Tanque
+#define nivel_max_sup 1740
+#define nivel_min_sup 465
 #define cap_sup (nivel_max_sup - nivel_min_sup)
 #define level_ledA_on (nivel_min_sup + cap_sup/4)
 #define level_ledA_off (nivel_min_sup + cap_sup * 3/4)
+
+// Cisterna
+#define nivel_max_inf 2040
+#define nivel_min_inf 707
+#define cap_inf (nivel_max_inf - nivel_min_inf)
+#define level_ledR_on (nivel_min_inf + cap_inf/4)
+#define level_ledR_off (nivel_min_inf + cap_inf * 3/4)
+#define level_bomba_on (nivel_min_inf + cap_inf/2)
 
 
 #endif 
--- adc_tp/main.c
+++ adc_tp/main.c
@@ -4,21 +4,66 @@
 #include "systick.h"
 #include "hardware.h"
 
+//Estados 
+typedef enum estado{
+    tanque_lleno,
+    tanque_medio,
+    tanque_vacio,
+    cisterna_llena
+}estado;
+
+//Antirebote del pulsador
+volatile uint32_t demora = 0;
+void puls_callback(uint gpio, uint32_t event_mask);
+
+void bomba_off();
+void bomba_on();
 
 int main()
 {
+    //Inicializaciones
     stdio_init_all();
     init_systick();
 
-    gpio_init(LED);
+    gpio_init_mask((1 << LEDR_PIN) | (1 << LEDA_PIN) | (1 << LEDV_PIN));
+    gpio_set_dir_out_masked((1 << LEDR_PIN) | (1 << LEDA_PIN) | (1 << LEDV_PIN));
+    gpio_clr_mask((1 << LEDR_PIN) | (1 << LEDA_PIN) | (1 << LEDV_PIN));
 
-    gpio_set_dir(PIN_LED, GPIO_OUT);
-    gpio_put(PIN_LED, 0);
+    gpio_init(PULS_PIN);
+    gpio_set_dir(PULS_PIN, GPIO_IN);
+    gpio_pull_up(PULS_PIN);
+
+    gpio_init(CTRL_BOMBA);
+    gpio_set_dir(CTRL_BOMBA, GPIO_OUT);
+    gpio_put(CTRL_BOMBA, 0);
 
     adc_init();
-    adc_gpio_init(ADC_0_GPIO);
-    adc_gpio_init(ADC_1_GPIO);
-    adc_select_input(ADC_CHANNEL_0);
+    adc_gpio_init(FLOT_BAJO_PIN);
+    adc_gpio_init(FLOT_ALTO_PIN);
 
+    //Configuración de función callback del pulsador
+    gpio_set_irq_enabled_with_callback(PULS_PIN, GPIO_IRQ_EDGE_FALL, true, puls_callback);
+
+    uint32_t estado_tanque = 0, estado_cisterna = 0;
     
+    adc_select_input(FLOT_BAJO_PIN);
+    estado_cisterna = adc_read();
+
+
+}
+
+//Función antirebote del pulsador
+void puls_callback(uint gpio, uint32_t event_mask) {
+    demora = get_systick() + REBOTE_PULS;
+} 
+
+// Funciones de la bomba
+void bomba_off(){
+    gpio_put(CTRL_BOMBA, 1);
+    gpio_put(LEDV_PIN, 0);
+}
+
+void bomba_on(){
+    gpio_put(CTRL_BOMBA, 0);
+    gpio_put(LEDV_PIN, 1);
 }
