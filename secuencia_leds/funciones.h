fe865b65f0a732829b6278ae05a60da31bce99b4
tomas <tomasgonzalez1291@gmal.com>
2025-09-08 08:45:27 -0300

    act

--- secuencia_leds/funciones.h
+++ secuencia_leds/funciones.h
@@ -1,27 +1,17 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-//Variables
+//Variables globales
+extern volatile bool puls_presionado;
+
 typedef struct{
-    uint8_t ledR_on          :1;
-    uint8_t ledA_on          :1;
-    uint8_t ledV_on          :1;
-    uint8_t reservado        :5;
+    uint8_t ledR_on   :1;
+    uint8_t ledA_on   :1;
+    uint8_t ledV_on   :1;
+    uint8_t reservado :5;
 }flags_s;
 
 extern flags_s flag;
-
-extern volatile bool puls_presionado;
-
-typedef enum{
-    rojo_on,
-    amarillo_on,
-    verde_on,
-    boton_pres
-}estados_e;
-
-extern estados_e estado_actual;
-extern uint32_t lapso;
 
 //Funciones globales
 void init_config(void);
